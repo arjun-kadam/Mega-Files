2777f10846e05573993108fc8cd1a34dbf7c1772
Arjun Kadam <arjunkadama29@gmail.com>
2024-09-20 21:50:01 +0530

    Removed service from Render.com

--- src/main/java/com/megafiles/config/JwtAuthenticationFilter.java
+++ src/main/java/com/megafiles/config/JwtAuthenticationFilter.java
@@ -32,7 +32,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
-        System.out.println(authHeader);
         final String jwt;
         final String userEmail;
 
